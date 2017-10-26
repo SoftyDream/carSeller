6819dc3e476b9247e15e6af8abc765ded5043ae3
Rojat <thomas.rojat1@gmail.com>
2017-10-26 09:57:44 +0200

    ajout de code

--- src/Models/Car.php
+++ src/Models/Car.php
@@ -6,7 +6,85 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model 
 {
+	private $chevaux;
+	private $longueur;
+	private $prix;
+	private $date_sortie;
+	private $model;
+	private $marque;
+	private $version;
 	
+	public function getChevaux()
+	{
+		return $this->chevaux;
+	}
+
+	public function getLongueur()
+	{
+		return $this->longueur;
+	}
+
+	public function getPrix()
+	{
+		return $this->prix;
+	}
+
+	public function getDate_Sortie()
+	{
+		return $this->date_sortie;
+	}
+
+	public function getModel()
+	{
+		return $this->model;
+	}
+
+	public function getMarque()
+	{
+		return $this->marque;
+	}
+
+	public function getVersion()
+	{
+		return $this->version;
+	}
+
+	public function setChevaux($chevaux)
+	{
+		$this->chevaux=$chevaux;
+	}
+
+	public function setLongueur($longueur)
+	{
+		$this->longueur=$longueur;
+	}
+
+	public function setPrix($prix)
+	{
+		$this->prix=$prix;
+	}
+
+	public function setDate_Sortie($date_sortie)
+	{
+		$this->date_sortie=$date_sortie;
+	}
+
+	public function setModel($model)
+	{
+		$this->model=$model;
+	}
+
+	public function setMarque($marque)
+	{
+		$this->marque=$marque;
+	}
+
+	public function setVersion($version)
+	{
+		$this->version=$version;
+	}
+
+		
 	
 	protected $fillable = ['title'];
 	public $timestamps = false;
--- src/routes.php
+++ src/routes.php
@@ -5,36 +5,51 @@
 	//GET Route
 
 
-	$app->get('/[{name}]', function ($request, $response, $args) {
-	//$app->get('/Car', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/[{name}]', function ($request, $response, $args)
+{
+	$this->logger->info("Slim-Skeleton '/' route");
+	// Render index view
+	//$this->db;
+	/*$capsule = new \Illuminate\Database\Capsule\Manager;
+	if (!$capscule::schema()->hasTable('merco'))
+	{
+		$capsule::schema()->create('merco', function (\Illuminate\Database\Schema\Blueprint $table)
+		{
+	 
+			$table->increments('id');
+        		$table->string('titre')->default('');
+			$table->int('chevaux');
+			$table->int('longueur');
+			$table->int('prix');
+			$table->string('date_sortie')->default('');
+			$table->string('model')->default('');
+			$table->string('marque')->default('');
+			$table->string('version')->default('');
+	
+        		//….
+        		$table->timestamps();
+		});
+	}*/
+	
 
-    // Render index view
-    return $this->renderer->render($response, 'page.phtml', $args);
-    //return $this->renderer->render($response, 'index.phtml', $args);
-    //return $this->renderer->render($response, 'thomas.phtml', $args);
+    	return $this->renderer->render($response, 'page.phtml', $args);
 });
 
+
+
+
+
+
+
 	
-	//POST Route
-	/*
-	$app->post('/[{name}]', function store($request, $response)  {
-	    
-	    return $this->renderer->render($response, 'thomas.phtml');
-	}); */
-	$app->post('/team_name_url/', function ($request, $response, $args)
-	{
-		
-    	// Récupération des variables POST
-    	foreach ( $_POST as $post => $val )  {            
-        	$$post = $val;
-    	}
- 	//var_dump($_POST);
- 
-  		 
-  	  	return $this->renderer->render($response, 'index.phtml', $_POST);;
-	});
+$app->post('/team_name_url/', function ($request, $response, $args)
+{
+	
+	
+	return $this->renderer->render($response, 'index.phtml', $_POST);;
+});
+
+
 
 
 	
